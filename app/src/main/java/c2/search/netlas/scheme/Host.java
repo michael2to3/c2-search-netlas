b76e5052577aebec41a10bc22b291a280ead6084
Michael <michael2ga3@gmail.com>
2023-03-02 20:44:17 +0300

    create Host - scheme describe target to send for check

---
+++ app/src/main/java/c2/search/netlas/scheme/Host.java
@@ -0,0 +1,27 @@
+package c2.search.netlas.scheme;
+
+public class Host {
+  private String host;
+  private int port;
+
+  public Host(String host, int port) {
+    this.host = host;
+    this.port = port;
+  }
+
+  public String getHost() {
+    return host;
+  }
+
+  public void setHost(String host) {
+    this.host = host;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+}
