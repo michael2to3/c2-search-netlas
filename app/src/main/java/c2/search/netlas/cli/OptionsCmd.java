8dbfbdc1974080b08793ecba52a88df1c02d6952
Michael <michael2ga3@gmail.com>
2023-03-02 20:43:33 +0300

    create OptionsCmd - describe options in cmd

---
+++ app/src/main/java/c2/search/netlas/cli/OptionsCmd.java
@@ -0,0 +1,36 @@
+package c2.search.netlas.cli;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+public class OptionsCmd {
+    public static Options get() {
+        Option setOption = Option.builder("s")
+                .longOpt("set")
+                .hasArg()
+                .argName("API")
+                .desc("Set the API to use for the application")
+                .build();
+
+        Option getOption = Option.builder("g")
+                .longOpt("get")
+                .hasArg()
+                .argName("OUTPUT")
+                .desc("Get the configuration for the specified output")
+                .build();
+
+        Option targetOption = Option.builder("t")
+                .longOpt("target")
+                .hasArg()
+                .argName("TARGET")
+                .desc("Set the target domain for the application")
+                .build();
+
+        Options optionsWithConfig = new Options();
+        optionsWithConfig.addOption(setOption);
+        optionsWithConfig.addOption(getOption);
+        optionsWithConfig.addOption(targetOption);
+
+        return optionsWithConfig;
+    }
+}
