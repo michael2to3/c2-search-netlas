9e73f3074d3a633870c2b10615b8bc3b34b911d6
Michael <michael2ga3@gmail.com>
2023-03-03 14:59:06 +0300

    add option port for target

--- app/src/main/java/c2/search/netlas/cli/OptionsCmd.java
+++ app/src/main/java/c2/search/netlas/cli/OptionsCmd.java
@@ -5,34 +5,38 @@ import org.apache.commons.cli.Options;
 
 public class OptionsCmd {
   public static Options get() {
-    Option setOption =
-        Option.builder("s")
-            .longOpt("set")
-            .hasArg()
-            .argName("API")
-            .desc("Set the API to use for the application")
-            .build();
+    Option setOption = Option.builder("s")
+        .longOpt("set")
+        .hasArg()
+        .argName("API")
+        .desc("Set the API to use for the application")
+        .build();
 
-    Option getOption =
-        Option.builder("g")
-            .longOpt("get")
-            .hasArg()
-            .argName("OUTPUT")
-            .desc("Get the configuration for the specified output")
-            .build();
+    Option getOption = Option.builder("g")
+        .longOpt("get")
+        .hasArg()
+        .argName("OUTPUT")
+        .desc("Get the configuration for the specified output")
+        .build();
 
-    Option targetOption =
-        Option.builder("t")
-            .longOpt("target")
-            .hasArg()
-            .argName("TARGET")
-            .desc("Set the target domain for the application")
-            .build();
+    Option targetOption = Option.builder("t")
+        .longOpt("target")
+        .hasArg()
+        .argName("TARGET")
+        .desc("Set the target domain for the application")
+        .build();
+    Option targetPortOption = Option.builder("p")
+        .longOpt("port")
+        .hasArg()
+        .argName("PORT")
+        .desc("Set the target port for the application")
+        .build();
 
     Options optionsWithConfig = new Options();
     optionsWithConfig.addOption(setOption);
     optionsWithConfig.addOption(getOption);
     optionsWithConfig.addOption(targetOption);
+    optionsWithConfig.addOption(targetPortOption);
 
     return optionsWithConfig;
   }
