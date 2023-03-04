feef529cb2697cb5b20a88569a7ad82b69cd9ed9
Michael <michael2ga3@gmail.com>
2023-03-04 17:14:32 +0300

    metasploit target - simple equals body response

--- app/src/main/java/c2/search/netlas/target/metasploit/Metasploit.java
+++ app/src/main/java/c2/search/netlas/target/metasploit/Metasploit.java
@@ -1,1 +1,17 @@
-package 
+package c2.search.netlas.target.metasploit;
+
+import c2.search.netlas.scheme.DetectResponse;
+import c2.search.netlas.scheme.Host;
+import c2.search.netlas.target.Target;
+
+public class Metasploit implements Target {
+
+  @Override
+  public DetectResponse run(final Host host) {
+    return null;
+  }
+
+  private boolean defaultBodyResponse(final Host host) {
+    return "<html><body><h1>It works!</h1></body></html>" == host.getHost();
+  }
+}
