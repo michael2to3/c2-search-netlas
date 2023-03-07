e0db2144373f81982dc84cdc0bd4934f02c35f48
Michael <michael2ga3@gmail.com>
2023-03-08 01:07:19 +0300

    add debug option

--- app/src/main/java/c2/search/netlas/cli/Cli.java
+++ app/src/main/java/c2/search/netlas/cli/Cli.java
@@ -1,6 +1,7 @@
 package c2.search.netlas.cli;
 
 import java.io.PrintStream;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -10,8 +11,11 @@ import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import c2.search.netlas.scheme.Host;
+import c2.search.netlas.target.metasploit.Metasploit;
+
 public class Cli {
-  private static final Logger logger = LoggerFactory.getLogger(Cli.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(Cli.class);
   private final Config config;
 
   public Cli() {
@@ -24,21 +28,34 @@ public class Cli {
 
     try {
       CommandLine cmd = parser.parse(optionsWithConfig, args);
+      if(cmd.hasOption("debug")) {
+
+      }
       if (cmd.hasOption("s")) {
-        logger.debug("Command: {}", cmd.getOptionValue("s"));
+        LOGGER.debug("Command: {}", cmd.getOptionValue("s"));
         config.setApi(cmd.getOptionValue("s"));
       }
       if (cmd.hasOption("g")) {
-        logger.debug("Command: {}", cmd.getOptionValue("g"));
+        LOGGER.debug("Command: {}", cmd.getOptionValue("g"));
         stream.println("Your API key is: " + config.getApi());
       }
+      Host host = new Host();
       if (cmd.hasOption("t")) {
-        logger.debug("Command: {}", cmd.getOptionValue("t"));
+        LOGGER.debug("Command: {}", cmd.getOptionValue("t"));
+        host.setHost(cmd.getOptionValue("t"));
       }
+      if(cmd.hasOption("p")) {
+        LOGGER.debug("Command: {}", cmd.getOptionValue("p"));
+        host.setPort(Integer.parseInt(cmd.getOptionValue("p")));
+      }
+
+      var m = new Metasploit().run(host);
+      System.out.println(m.toString());
+
     } catch (ParseException e) {
       System.out.println("Error: " + e.getMessage());
       HelpFormatter formatter = new HelpFormatter();
-      formatter.printHelp("myapp", optionsWithConfig);
+      formatter.printHelp("c2detect", optionsWithConfig);
     } catch (Exception e) {
       e.printStackTrace();
     }
