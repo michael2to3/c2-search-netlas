a267bb4669e90efbec816d089edf03f16a99a3e9
Michael <michael2ga3@gmail.com>
2023-03-08 01:06:29 +0300

    add simple check body response

--- app/src/main/java/c2/search/netlas/target/metasploit/Metasploit.java
+++ app/src/main/java/c2/search/netlas/target/metasploit/Metasploit.java
@@ -1,17 +1,65 @@
 package c2.search.netlas.target.metasploit;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import c2.search.netlas.scheme.DetectResponse;
 import c2.search.netlas.scheme.Host;
 import c2.search.netlas.target.Target;
 
 public class Metasploit implements Target {
+  private final static Logger LOGGER = LoggerFactory.getLogger(Metasploit.class);
 
   @Override
   public DetectResponse run(final Host host) {
-    return null;
+    DetectResponse response = new DetectResponse();
+
+    response.setName("Metasploit");
+    response.setMinVersion("6.3.2");
+    response.setMaxVersion("6.3.2");
+    response.setCountAllTest(1);
+
+    int count = 0;
+    count += checkDefaultBodyResponse(host) ? 1 : 0;
+
+    response.setCountSuccessTest(count);
+    return response;
   }
 
-  private boolean defaultBodyResponse(final Host host) {
-    return "<html><body><h1>It works!</h1></body></html>" == host.getHost();
+  private boolean checkDefaultBodyResponse(final Host host) {
+    String body = "";
+    try {
+      body = getBodyResponse(host);
+    } catch (IOException e) {
+      LOGGER.error(e.getMessage());
+      return false;
+    }
+    String defaultBody = "<html><body><h1>It works!</h1></body></html>";
+    return body.equals(defaultBody);
+  }
+
+  private String getBodyResponse(final Host host) throws IOException {
+    URL url = new URL("http://" + host.getHost() + ":" + host.getPort() + "/");
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+    String inputLine;
+    StringBuilder content = new StringBuilder();
+
+    while ((inputLine = in.readLine()) != null) {
+      content.append(inputLine);
+    }
+
+    in.close();
+    con.disconnect();
+
+    return content.toString();
   }
 }
