e6806c246dc26d023ae5ee31b57be2697c9b4166
Michael <michael2ga3@gmail.com>
2023-03-09 11:35:17 +0300

    wrapper now have global cache for all resource

--- app/src/main/java/c2/search/netlas/target/NetlasWrapper.java
+++ app/src/main/java/c2/search/netlas/target/NetlasWrapper.java
@@ -5,20 +5,24 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import netlas.java.APIException;
+import java.util.Map;
 import netlas.java.Netlas;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-class NetlasWrapper {
+public class NetlasWrapper {
+  private static final Logger LOGGER = LoggerFactory.getLogger(NetlasWrapper.class);
+  private static Map<Host, JsonNode> response;
   private Host host;
   private Netlas netlas;
-  private JsonNode response;
 
   public NetlasWrapper(final String api, final Host host)
-      throws JsonMappingException, JsonProcessingException, APIException {
+      throws JsonMappingException, JsonProcessingException {
     this.netlas = new Netlas(api);
     this.host = host;
-    getResponse();
+    response = new HashMap<>();
   }
 
   public Host getHost() {
@@ -37,35 +41,48 @@ class NetlasWrapper {
     this.netlas = netlas;
   }
 
-  public void setResponse(JsonNode response) {
-    this.response = response;
+  public void setResponse(JsonNode json) {
+    if (response.containsKey(host)) {
+      response.replace(host, json);
+    } else {
+      response.put(host, json);
+    }
   }
 
-  public List<String> getDnsName() {
-    var commonName =
-        this.response
-            .get("items")
-            .get(0)
-            .get("data")
-            .get("certificate")
-            .get("subject")
-            .get("common_name");
+  public List<String> getDnsName() throws JsonMappingException, JsonProcessingException {
+    var commonName = getResponse().get("data").get("certificate").get("subject").get("common_name");
     List<String> dnsNames = new ArrayList<String>();
     commonName.forEach(item -> dnsNames.add(item.asText()));
     return dnsNames;
   }
 
-  private void getResponse() throws JsonMappingException, JsonProcessingException, APIException {
-    if (response != null) {
-      return;
+  public String getJarm() throws JsonMappingException, JsonProcessingException {
+    return getResponse().get("data").get("jarm").asText();
+  }
+
+  public String getResponseBody() throws JsonMappingException, JsonProcessingException {
+    return getResponse().get("data").get("http").get("body").asText();
+  }
+
+  public String getResponseBodyAsSha256() throws JsonMappingException, JsonProcessingException {
+    return getResponse().get("data").get("http").get("body_sha256").asText();
+  }
+
+  public JsonNode getResponse() throws JsonMappingException, JsonProcessingException {
+    if (response.containsKey(host)) {
+      return response.get(host);
     }
 
-    var query = host.getHost();
-    var datatype = "response";
+    var query = String.format("host:%s AND port:%s", host.getTarget(), host.getPort());
+    var datatype = "responses";
     var page = 0;
     var indices = "";
     var fields = "";
     var excludeFields = false;
-    this.response = this.netlas.search(query, datatype, page, indices, fields, excludeFields);
+    var resp = this.netlas.search(query, datatype, page, indices, fields, excludeFields);
+    resp = resp.get("items").get(0);
+    setResponse(resp);
+
+    return resp;
   }
 }
