2ef4a5ec9c4ae5e1a1a4e50178824cca9db78175
Michael <michael2ga3@gmail.com>
2023-03-09 11:34:39 +0300

    add more tests for metasploit

--- app/src/main/java/c2/search/netlas/target/metasploit/Metasploit.java
+++ app/src/main/java/c2/search/netlas/target/metasploit/Metasploit.java
@@ -2,38 +2,47 @@ package c2.search.netlas.target.metasploit;
 
 import c2.search.netlas.scheme.DetectResponse;
 import c2.search.netlas.scheme.Host;
+import c2.search.netlas.target.NetlasWrapper;
 import c2.search.netlas.target.Target;
-import java.io.BufferedReader;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Metasploit implements Target {
   private static final Logger LOGGER = LoggerFactory.getLogger(Metasploit.class);
+  private static final String name = "Metasploit";
+  private String maxVersion = "6.x.x";
+  private String minVersion = "x.x.x";
+  private Host host;
+  private NetlasWrapper netlas;
 
   @Override
-  public DetectResponse run(final Host host) {
+  public void setNetlas(final NetlasWrapper netlas) {
+    this.netlas = netlas;
+  }
+
+  @Override
+  public DetectResponse run() {
     DetectResponse response = new DetectResponse();
 
-    response.setName("Metasploit");
-    response.setMinVersion("6.3.2");
-    response.setMaxVersion("6.3.2");
-    response.setCountAllTest(1);
+    response.setName(name);
+    response.setMaxVersion(maxVersion);
+    response.setMinVersion(minVersion);
+    response.setCountAllTest(2);
 
     int count = 0;
-    count += checkDefaultBodyResponse(host) ? 1 : 0;
+    count += checkDefaultBodyResponse() ? 1 : 0;
+    count += checkJarm() ? 1 : 0;
 
     response.setCountSuccessTest(count);
     return response;
   }
 
-  private boolean checkDefaultBodyResponse(final Host host) {
+  private boolean checkDefaultBodyResponse() {
     String body = "";
     try {
-      body = getBodyResponse(host);
+      body = netlas.getResponseBody();
     } catch (IOException e) {
       LOGGER.error(e.getMessage());
       return false;
@@ -42,22 +51,42 @@ public class Metasploit implements Target {
     return body.equals(defaultBody);
   }
 
-  private String getBodyResponse(final Host host) throws IOException {
-    URL url = new URL("http://" + host.getHost() + ":" + host.getPort() + "/");
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestMethod("GET");
+  boolean checkJarm() {
+    String jarmv5 = "07d14d16d21d21d07c42d43d000000f50d155305214cf247147c43c0f1a823";
+    String jarmv6 = "07d19d12d21d21d07c42d43d000000f50d155305214cf247147c43c0f1a823";
 
-    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-    String inputLine;
-    StringBuilder content = new StringBuilder();
-
-    while ((inputLine = in.readLine()) != null) {
-      content.append(inputLine);
+    String responseJarm = "";
+    try {
+      responseJarm = netlas.getJarm();
+    } catch (JsonProcessingException e) {
+      LOGGER.error(e.getMessage());
     }
 
-    in.close();
-    con.disconnect();
+    String minVersion = "6.x.x";
+    boolean detect = false;
+    if (responseJarm.equals(jarmv5)) {
+      minVersion = "5.x.x";
+      detect = true;
+    }
+    if (responseJarm.equals(jarmv6)) {
+      minVersion = "6.x.x";
+      detect = true;
+    }
+    changeMinimalVersion(minVersion);
+    return detect;
+  }
 
-    return content.toString();
+  private void changeMinimalVersion(String version) {
+    if (version.compareTo(minVersion) < 0) {
+      minVersion = version;
+    }
+  }
+
+  private boolean checkUA() {
+    String defaultUA =
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko)"
+            + " Version/16.1 Safari/605.1.15";
+
+    return false;
   }
 }
