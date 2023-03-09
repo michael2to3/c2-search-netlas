3d6b22610276209520456d09b96aa4c9e65ea715
Michael <michael2ga3@gmail.com>
2023-03-09 11:33:56 +0300

    refactoring try catch

--- app/src/main/java/c2/search/netlas/cli/Cli.java
+++ app/src/main/java/c2/search/netlas/cli/Cli.java
@@ -1,7 +1,9 @@
 package c2.search.netlas.cli;
 
 import c2.search.netlas.scheme.Host;
+import c2.search.netlas.target.NetlasWrapper;
 import c2.search.netlas.target.metasploit.Metasploit;
+import java.io.IOException;
 import java.io.PrintStream;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -20,41 +22,43 @@ public class Cli {
     this.config = new Config();
   }
 
-  public void run(final PrintStream stream, String[] args) {
+  public void run(final PrintStream stream, String[] args) throws IOException {
     Options optionsWithConfig = OptionsCmd.get();
     CommandLineParser parser = new DefaultParser();
 
+    CommandLine cmd = null;
     try {
-      CommandLine cmd = parser.parse(optionsWithConfig, args);
-      if (cmd.hasOption("debug")) {}
-
-      if (cmd.hasOption("s")) {
-        LOGGER.debug("Command: {}", cmd.getOptionValue("s"));
-        config.setApi(cmd.getOptionValue("s"));
-      }
-      if (cmd.hasOption("g")) {
-        LOGGER.debug("Command: {}", cmd.getOptionValue("g"));
-        stream.println("Your API key is: " + config.getApi());
-      }
-      Host host = new Host();
-      if (cmd.hasOption("t")) {
-        LOGGER.debug("Command: {}", cmd.getOptionValue("t"));
-        host.setHost(cmd.getOptionValue("t"));
-      }
-      if (cmd.hasOption("p")) {
-        LOGGER.debug("Command: {}", cmd.getOptionValue("p"));
-        host.setPort(Integer.parseInt(cmd.getOptionValue("p")));
-      }
-
-      var m = new Metasploit().run(host);
-      System.out.println(m.toString());
-
+      cmd = parser.parse(optionsWithConfig, args);
     } catch (ParseException e) {
       System.out.println("Error: " + e.getMessage());
       HelpFormatter formatter = new HelpFormatter();
       formatter.printHelp("c2detect", optionsWithConfig);
-    } catch (Exception e) {
-      e.printStackTrace();
     }
+    if (cmd.hasOption("debug")) {}
+
+    if (cmd.hasOption("s")) {
+      LOGGER.debug("Command: {}", cmd.getOptionValue("s"));
+      config.setApi(cmd.getOptionValue("s"));
+    }
+    if (cmd.hasOption("g")) {
+      LOGGER.debug("Command: {}", cmd.getOptionValue("g"));
+      stream.println("Your API key is: " + config.getApi());
+    }
+    Host host = new Host();
+    if (cmd.hasOption("t")) {
+      LOGGER.debug("Command: {}", cmd.getOptionValue("t"));
+      host.setTarget(cmd.getOptionValue("t"));
+    }
+    if (cmd.hasOption("p")) {
+      LOGGER.debug("Command: {}", cmd.getOptionValue("p"));
+      host.setPort(Integer.parseInt(cmd.getOptionValue("p")));
+    }
+
+    var api = System.getProperty("KEY_API");
+    var netlas = new NetlasWrapper(api, host);
+    var m = new Metasploit();
+    m.setNetlas(netlas);
+    m.run();
+    System.out.println(m.toString());
   }
 }
