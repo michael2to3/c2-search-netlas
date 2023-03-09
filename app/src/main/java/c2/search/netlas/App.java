30cab73381826b3cada206f813d8b8865f1f0bd1
Michael <michael2ga3@gmail.com>
2023-03-09 11:33:22 +0300

    try catch move to app class

--- app/src/main/java/c2/search/netlas/App.java
+++ app/src/main/java/c2/search/netlas/App.java
@@ -1,10 +1,15 @@
 package c2.search.netlas;
 
 import c2.search.netlas.cli.Cli;
+import java.io.IOException;
 
 public class App {
 
   public static void main(String[] args) {
-    new Cli().run(System.out, args);
+    try {
+      new Cli().run(System.out, args);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
