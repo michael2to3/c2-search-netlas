19d6fbec01c34c3debe378824de204286c40b935
Michael <michael2ga3@gmail.com>
2023-03-10 17:09:25 +0300

    create annotation for common interface detection test

---
+++ app/src/main/java/c2/search/netlas/annotation/Detect.java
@@ -0,0 +1,12 @@
+package c2.search.netlas.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Detect {
+  String name() default "";
+}
---
+++ app/src/main/java/c2/search/netlas/annotation/Test.java
@@ -0,0 +1,11 @@
+package c2.search.netlas.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+}
