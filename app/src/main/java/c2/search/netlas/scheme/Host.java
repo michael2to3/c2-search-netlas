7698352dc11f1535c56b7c9c5795837b703c137c
Michael <michael2ga3@gmail.com>
2023-03-10 17:10:28 +0300

    add toString for host class

--- app/src/main/java/c2/search/netlas/scheme/Host.java
+++ app/src/main/java/c2/search/netlas/scheme/Host.java
@@ -11,6 +11,10 @@ public class Host {
     this.port = port;
   }
 
+  @Override public String toString() {
+    return String.format("%s:%d", target, port);
+  }
+
   public String getTarget() {
     return target;
   }
