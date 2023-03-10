1ed3c1fcc98c1644340a8f903d70630d2990473b
Michael <michael2ga3@gmail.com>
2023-03-10 17:12:47 +0300

    add support bad status exit

--- app/src/main/java/c2/search/netlas/App.java
+++ app/src/main/java/c2/search/netlas/App.java
@@ -1,15 +1,18 @@
 package c2.search.netlas;
 
 import c2.search.netlas.cli.Cli;
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App {
+  private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
   public static void main(String[] args) {
     try {
       new Cli().run(System.out, args);
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage(), e);
+      System.exit(1);
     }
   }
 }
