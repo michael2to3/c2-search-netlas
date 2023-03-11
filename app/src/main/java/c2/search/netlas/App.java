3f40a2f99862177302f20697108c6ce7d6ed3153
Michael <michael2ga3@gmail.com>
2023-03-11 07:58:20 +0300

    now api key get from config

--- app/src/main/java/c2/search/netlas/App.java
+++ app/src/main/java/c2/search/netlas/App.java
@@ -1,11 +1,13 @@
 package c2.search.netlas;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import c2.search.netlas.cli.Cli;
+import c2.search.netlas.cli.Config;
 import c2.search.netlas.scheme.Host;
 import c2.search.netlas.target.Checker;
 import c2.search.netlas.target.NetlasWrapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class App {
   private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
@@ -18,7 +20,9 @@ public class App {
         return;
       }
 
-      var api = System.getenv("API_KEY");
+      var config = new Config("config.properties");
+      var api = config.get("api.key");
+      System.out.println(api);
       var netlas = new NetlasWrapper(api, host);
       var reponses = new Checker(netlas, host).run();
       System.out.println(reponses);
