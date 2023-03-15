ee7cd449f0d567c322c5ade0797019c093254276
Michael <michael2ga3@gmail.com>
2023-03-16 00:10:01 +0300

    add unit test for scan with annotation

--- app/src/test/java/c2/search/netlas/classscanner/ClassScannerTest.java
+++ app/src/test/java/c2/search/netlas/classscanner/ClassScannerTest.java
@@ -2,12 +2,12 @@ package c2.search.netlas.classscanner;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import c2.search.netlas.annotation.*;
 import java.io.IOException;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ClassScannerTest {
-
   @Test
   void testGetClasses() throws ClassNotFoundException, IOException {
     String packageName = "c2.search.netlas.classscanner";
@@ -22,6 +22,14 @@ class ClassScannerTest {
     String packageName = "c2.search.netlas";
     ClassScanner scanner = new ClassScanner(packageName);
     List<Class<?>> classes = scanner.getClasses();
+    assertTrue(classes.size() > 0);
+  }
+
+  @Test
+  void testScanClassesWithAnnotation() throws ClassNotFoundException, IOException {
+    String packageName = "c2.search.netlas";
+    ClassScanner scanner = new ClassScanner(packageName);
+    List<Class<?>> classes = scanner.getClassesWithAnnotation(Detect.class);
     assertTrue(classes.size() > 0);
   }
 
