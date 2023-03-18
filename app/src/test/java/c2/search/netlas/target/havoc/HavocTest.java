ff81e1b81a2b85cf1668172a1040a3deae79495a
Michael <michael2ga3@gmail.com>
2023-03-18 22:06:31 +0300

    create unit test for detect havoc

---
+++ app/src/test/java/c2/search/netlas/target/havoc/HavocTest.java
@@ -0,0 +1,62 @@
+package c2.search.netlas.target.havoc;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import c2.search.netlas.scheme.Host;
+import c2.search.netlas.scheme.Response;
+import c2.search.netlas.target.NetlasWrapper;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class HavocTest {
+  @Mock private Host host;
+  @Mock private NetlasWrapper netlasWrapper;
+  @InjectMocks private Havoc havoc;
+
+  @BeforeEach
+  public void setUp() {
+    havoc.host = host;
+    havoc.netlasWrapper = netlasWrapper;
+  }
+
+  @Test
+  @DisplayName("Test checkDefaultBodyResponse() with non-default body")
+  public void testCheckDefaultBodyResponseWithNonDefaultBody() throws IOException {
+    String nonDefaultBody = "<html><body><h1>It doesn't work!</h1></body></html>";
+    when(netlasWrapper.getBody()).thenReturn(nonDefaultBody);
+    Response response = havoc.checkDefaultBodyResponse();
+    assertNotNull(response);
+    assertFalse(response.isSuccess());
+  }
+
+  @Test
+  @DisplayName("Test checkJarm() with JARM")
+  public void testCheckJarmWithJarmV6() throws IOException {
+    String jarm = "3fd21b20d00000021c43d21b21b43de0a012c76cf078b8d06f4620c2286f5e";
+    when(netlasWrapper.getJarm()).thenReturn(jarm);
+    Response response = havoc.checkJarm();
+    assertNotNull(response);
+    assertTrue(response.isSuccess());
+  }
+
+  @Test
+  public void testCheckDefaultBodyResponse() throws Exception {
+    when(netlasWrapper.getBody()).thenReturn("404 page not found");
+    when(netlasWrapper.getStatusCode()).thenReturn(404);
+    when(netlasWrapper.getServers()).thenReturn(new ArrayList<>());
+    Response response = havoc.checkDefaultBodyResponse();
+    assertNotNull(response);
+    assertTrue(response.isSuccess());
+  }
+}
