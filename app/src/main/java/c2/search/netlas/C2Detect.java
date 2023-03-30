c1101f6728cb56502498d128ae0f288516d8422b
Michael <michael2ga3@gmail.com>
2023-03-30 07:00:09 +0300

    up to date C2Detect, use more flex interface

--- app/src/main/java/c2/search/netlas/C2Detect.java
+++ app/src/main/java/c2/search/netlas/C2Detect.java
@@ -1,10 +1,14 @@
 package c2.search.netlas;
 
+import c2.search.netlas.classscanner.AnnotatedFieldValues;
+import c2.search.netlas.classscanner.Checker;
 import c2.search.netlas.cli.Config;
 import c2.search.netlas.scheme.Host;
 import c2.search.netlas.scheme.Results;
-import c2.search.netlas.target.Checker;
 import c2.search.netlas.target.NetlasWrapper;
+import java.io.IOException;
+import java.net.Socket;
+import netlas.java.Netlas;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -14,16 +18,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class C2Detect {
-  private final Logger logger = LoggerFactory.getLogger(C2Detect.class);
+  private final Logger LOGGER = LoggerFactory.getLogger(C2Detect.class);
   private final Config config;
   private final Options options;
+  private static final int DEFAULT_SOCKET_TIMEOUT = 1000;
 
   public C2Detect(Config config, Options options) {
+    LOGGER.info("Initializing C2Detect with config: {}, options: {}", config, options);
     this.config = config;
     this.options = options;
   }
 
   public void run(String[] args) throws Exception {
+    LOGGER.info("Running C2Detect");
+
     CommandLineParser parser = new DefaultParser();
     CommandLine cmd = parser.parse(options, args);
 
@@ -46,8 +54,37 @@ public class C2Detect {
 
     Host host = createHost(cmd);
     NetlasWrapper netlas = new NetlasWrapper(apiKey, host);
-    Results responses = new Checker(netlas, host).run();
+
+    AnnotatedFieldValues fields = new AnnotatedFieldValues();
+    fields.setField(Host.class, host);
+    fields.setField(NetlasWrapper.class, netlas);
+    fields.setField(Netlas.class, netlas.getNetlas());
+    fields.setField(Socket.class, getSocket(host, getSocketTimeout(DEFAULT_SOCKET_TIMEOUT)));
+
+    Results responses = new Checker(fields).run();
     responses.print(System.out, verbose);
+  }
+
+  private int getSocketTimeout(int defaultTimeout) {
+    String timeout = config.get("socket.timeout");
+    if (timeout == null) {
+      return defaultTimeout;
+    }
+    try {
+      return Integer.parseInt(timeout);
+    } catch (NumberFormatException e) {
+      return defaultTimeout;
+    }
+  }
+
+  private Socket getSocket(Host host, int socketTimeout) {
+    try {
+      Socket socket = new Socket(host.getTarget(), host.getPort());
+      socket.setSoTimeout(socketTimeout);
+      return socket;
+    } catch (IOException e) {
+      return null;
+    }
   }
 
   protected static Host createHost(CommandLine cmd) {
