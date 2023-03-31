22900d244182df88478a840d85cbc6d6c44d4b50
Michael <michael2ga3@gmail.com>
2023-04-01 00:48:25 +0300

    split c2detect on parse args and work config and run and setup checker

--- app/src/main/java/c2/search/netlas/C2Detect.java
+++ app/src/main/java/c2/search/netlas/C2Detect.java
@@ -2,7 +2,6 @@ package c2.search.netlas;
 
 import c2.search.netlas.classscanner.AnnotatedFieldValues;
 import c2.search.netlas.classscanner.Checker;
-import c2.search.netlas.cli.Config;
 import c2.search.netlas.cli.ParseCmdArgs;
 import c2.search.netlas.scheme.Host;
 import c2.search.netlas.scheme.Results;
@@ -12,30 +11,21 @@ import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
 import netlas.java.Netlas;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class C2Detect {
-  private static final int DEFAULT_SOCKET_TIMEOUT_MS = 1000;
   private final Logger LOGGER = LoggerFactory.getLogger(C2Detect.class);
-  private final Options options;
   private AnnotatedFieldValues fields;
-  private Host host;
   private NetlasWrapper netlas;
   private PrintStream stream;
   private Checker checker;
   private ParseCmdArgs cmd;
 
-  public C2Detect(ParseCmdArgs cmd, Options options, PrintStream stream) {
+  public C2Detect(ParseCmdArgs cmd, PrintStream stream) {
     LOGGER.info("Initializing C2Detect");
     this.cmd = cmd;
-    this.options = options;
     this.stream = stream;
   }
 
@@ -44,13 +34,9 @@ public class C2Detect {
   }
 
   public void setup(String[] args) throws IOException, ParseException, ClassNotFoundException {
-    CommandLineParser parser = new DefaultParser();
-    CommandLine cmd = parser.parse(options, args);
-
-    parseCommandLineArgs(cmd);
-    if (host != null) {
-      this.netlas = new NetlasWrapper(apiKey, host);
-      this.fields = createFields(host, netlas);
+    if (cmd.getHost() != null) {
+      this.netlas = new NetlasWrapper(cmd.getApiKey(), cmd.getHost());
+      this.fields = createFields(cmd.getHost(), netlas);
       this.checker = createChecker(fields);
     }
   }
@@ -67,7 +53,7 @@ public class C2Detect {
       throws IllegalAccessException, InstantiationException, InvocationTargetException,
           NoSuchMethodException, SecurityException {
     LOGGER.info("Running C2Detect");
-    if (host != null) {
+    if (cmd.getHost() != null) {
       runChecker();
     }
   }
@@ -77,44 +63,11 @@ public class C2Detect {
     return new Checker(fields);
   }
 
-  protected void printHelp() {
-    HelpFormatter formatter = new HelpFormatter();
-    formatter.printHelp("c2detect", options);
-  }
-
   private void runChecker()
       throws IllegalAccessException, InstantiationException, InvocationTargetException,
           NoSuchMethodException, SecurityException {
     Results responses = checker.run();
-    printResponses(responses, verbose);
-  }
-
-  private void parseCommandLineArgs(CommandLine cmd) {
-    if (cmd.hasOption("h")) {
-      printHelp();
-      return;
-    }
-
-    if (cmd.hasOption("s")) {
-      setApiKey(cmd.getOptionValue("s"));
-      return;
-    }
-
-    this.verbose = cmd.hasOption("v");
-    this.apiKey = getApiKey();
-    this.host = createHost(cmd);
-  }
-
-  private void setApiKey(String apiKey) {
-    config.save("api.key", apiKey);
-  }
-
-  private String getApiKey() {
-    String apiKey = config.get("api.key");
-    if (apiKey == null || apiKey.isEmpty()) {
-      throw new IllegalArgumentException("API key is required");
-    }
-    return apiKey;
+    printResponses(responses, cmd.isVerbose());
   }
 
   private AnnotatedFieldValues createFields(Host host, NetlasWrapper netlas) {
@@ -122,24 +75,12 @@ public class C2Detect {
     fields.setField(Host.class, host);
     fields.setField(NetlasWrapper.class, netlas);
     fields.setField(Netlas.class, netlas.getNetlas());
-    fields.setField(Socket.class, getSocket(host, getSocketTimeoutMs(DEFAULT_SOCKET_TIMEOUT_MS)));
+    fields.setField(Socket.class, getSocket(host, cmd.getSocketTimeoutMs()));
     return fields;
   }
 
   private void printResponses(Results responses, boolean verbose) {
     responses.print(stream, verbose);
-  }
-
-  private int getSocketTimeoutMs(int defaultTimeout) {
-    String timeout = config.get("socket.timeout");
-    if (timeout == null) {
-      return defaultTimeout;
-    }
-    try {
-      return Integer.parseInt(timeout);
-    } catch (NumberFormatException e) {
-      return defaultTimeout;
-    }
   }
 
   private Socket getSocket(Host host, int socketTimeout) {
