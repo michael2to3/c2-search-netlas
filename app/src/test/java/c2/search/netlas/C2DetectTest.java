4154a824d5f55b332fac6fcf364e0b132954b529
Michael <michael2ga3@gmail.com>
2023-04-01 00:48:37 +0300

    remove old methods from tests

--- app/src/test/java/c2/search/netlas/C2DetectTest.java
+++ app/src/test/java/c2/search/netlas/C2DetectTest.java
@@ -1,20 +1,10 @@
 package c2.search.netlas;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mockStatic;
 
-import c2.search.netlas.classscanner.AnnotatedFieldValues;
-import c2.search.netlas.classscanner.Checker;
 import c2.search.netlas.cli.Config;
-import c2.search.netlas.scheme.Host;
 import c2.search.netlas.scheme.Response;
 import c2.search.netlas.scheme.Results;
 import c2.search.netlas.scheme.Version;
@@ -24,7 +14,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.cli.CommandLine;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -34,26 +23,6 @@ public class C2DetectTest {
   @BeforeAll
   public static void setup() {
     mockStatic(Config.class);
-  }
-
-  @Test
-  public void testCreateHost() {
-    CommandLine cmd = mock(CommandLine.class);
-    when(cmd.getOptionValue("t")).thenReturn("example.com");
-    when(cmd.getOptionValue("p")).thenReturn("8080");
-
-    Host host = C2Detect.createHost(cmd);
-
-    assertNotNull(host);
-    assertEquals("example.com", host.getTarget());
-    assertEquals(8080, host.getPort());
-
-    when(cmd.getOptionValue("t")).thenReturn(null);
-    assertThrows(IllegalArgumentException.class, () -> C2Detect.createHost(cmd));
-
-    when(cmd.getOptionValue("t")).thenReturn("google.com");
-    when(cmd.getOptionValue("p")).thenReturn("asd");
-    assertThrows(IllegalArgumentException.class, () -> C2Detect.createHost(cmd));
   }
 
   @Test
@@ -85,45 +54,5 @@ public class C2DetectTest {
 
     Results results = new Results(responses);
     return results;
-  }
-
-  @Test
-  public void testChangeSettings() throws Exception {
-    String args[] = new String[] {"-s", "test.test"};
-    Config configMock = mock(Config.class);
-    C2Detect c2 = new C2Detect(configMock, App.setupOptions(), System.out);
-    Checker checker = mock(Checker.class);
-    when(checker.run()).thenReturn(getResults());
-
-    c2.run(args);
-    verify(configMock).save("api.key", "test.test");
-  }
-
-  @Test
-  public void testWithoutApi() throws Exception {
-    String args[] = new String[] {"-v", "-t", "google.com", "-p", "443"};
-    Config configMock = mock(Config.class);
-    when(configMock.get("api.key")).thenReturn(null);
-    C2Detect c2 = new C2Detect(configMock, App.setupOptions(), System.out);
-    assertThrows(Exception.class, () -> c2.run(args));
-  }
-
-  @Test
-  public void testWithApi() throws Exception {
-    String args[] = new String[] {"-v", "-t", "google.com", "-p", "443"};
-    Config configMock = mock(Config.class);
-    when(configMock.get("api.key")).thenReturn(API);
-
-    Checker checker = mock(Checker.class);
-    when(checker.run()).thenReturn(getResults());
-
-    C2Detect c2 = new C2Detect(configMock, App.setupOptions(), System.out);
-    c2.setChecker(checker);
-
-    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(outContent));
-
-    c2.run(args);
-    verify(checker).run();
   }
 }
