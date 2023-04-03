d3bca8e9e560c1c8fb09510753adf3f942106dcf
Michael <michael2ga3@gmail.com>
2023-04-03 17:54:49 +0300

    create full method app class

--- app/src/test/java/c2/search/netlas/AppTest.java
+++ app/src/test/java/c2/search/netlas/AppTest.java
@@ -1,23 +1,56 @@
 package c2.search.netlas;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.withSettings;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.HelpFormatter;
+import c2.search.netlas.cli.Config;
+import c2.search.netlas.cli.ParseCmdArgs;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
-
   @Test
   void printHelp() {
-    CommandLine mockCmd = mock(CommandLine.class);
-    when(mockCmd.hasOption("h")).thenReturn(true);
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(outputStream);
+    System.setOut(printStream);
 
     App.main(new String[] {"-h"});
+    assertTrue(outputStream.toString().contains("c2detect"));
+  }
 
-    HelpFormatter formatter = new HelpFormatter();
-    verify(formatter).printHelp("c2detect", App.setupOptions());
+  @Test
+  void testChangeSettings() {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(outputStream);
+    System.setOut(printStream);
+    Config config = mock(Config.class, withSettings().useConstructor("test.prop"));
+    App.setConfig(config);
+
+    App.main(new String[] {"-s", "api"});
+
+    verify(config).save("api.key", "api");
+  }
+
+  @Test
+  void testC2DetectRunInvocation() throws Exception {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(outputStream);
+    System.setOut(printStream);
+
+    String[] args = new String[] {"-t", "example.com", "-p", "80"};
+    ParseCmdArgs pargs = App.getParseCmdArgs(args);
+    C2Detect c2Detect = spy(new C2Detect(pargs, printStream));
+    doNothing().when(c2Detect).run();
+
+    App.setC2Detect(c2Detect);
+    App.startScan(args);
+
+    verify(c2Detect).run(args);
   }
 }
