eb113a11e8d7ae8fabc462830f1694ac78821058
Michael <michael2ga3@gmail.com>
2023-04-04 14:25:30 +0300

    use lib graph class to scan classes in build time

--- app/src/main/java/c2/search/netlas/classscanner/ClassScanner.java
+++ app/src/main/java/c2/search/netlas/classscanner/ClassScanner.java
@@ -1,17 +1,10 @@
 package c2.search.netlas.classscanner;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ScanResult;
 import java.lang.annotation.Annotation;
-import java.net.URL;
-import java.security.CodeSource;
-import java.security.ProtectionDomain;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.jar.JarEntry;
-import java.util.jar.JarInputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,107 +12,36 @@ public class ClassScanner {
   private static final Logger LOGGER = LoggerFactory.getLogger(ClassScanner.class);
   private final List<Class<?>> classes;
 
-  public ClassScanner(String packageName) throws IOException, ClassNotFoundException {
+  public ClassScanner(String packageName) {
     classes = new ArrayList<>();
-    String path = formatPath(packageName);
-    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-    InputStream inputStream = classLoader.getResourceAsStream(path);
-    if (inputStream == null) {
-      throw new IllegalArgumentException("Package not found: " + packageName);
-    }
-    if (checkJar()) {
-      JarInputStream jarInputStream = new JarInputStream(inputStream);
-      scanClasses(jarInputStream, packageName);
-    } else {
-      scanClasses(inputStream, packageName);
+    try (ScanResult scanResult =
+        new ClassGraph().enableAllInfo().whitelistPackages(packageName).scan()) {
+      scanResult.getAllClasses().loadClasses().forEach(this::addClass);
     }
   }
 
-  protected void scanClasses(InputStream inputStream, String packageName) throws IOException {
-    LOGGER.info("Scanning Dir classes in package: {}", packageName);
-    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-    String line;
-    while ((line = reader.readLine()) != null) {
-      if (line.endsWith(".class")) {
-        String className = packageName + "." + line.substring(0, line.length() - 6);
-        addClass(className, Thread.currentThread().getContextClassLoader());
-      } else {
-        String subPackageName = packageName + "." + line;
-        scanSubPackage(subPackageName);
-      }
-    }
-  }
-
-  protected void scanSubPackage(String packageName) throws IOException {
-    String path = formatPath(packageName);
-    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-    InputStream inputStream = classLoader.getResourceAsStream(path);
-    scanClasses(inputStream, packageName);
-  }
-
-  protected void scanClasses(JarInputStream jarInputStream, String packageName) throws IOException {
-    LOGGER.info("Scanning Jar classes in package: {}", packageName);
-    JarEntry entry;
-    while ((entry = jarInputStream.getNextJarEntry()) != null) {
-      LOGGER.debug("Jar entry: {}", entry.getName());
-      if (entry.getName().endsWith(".class")) {
-        LOGGER.info("Class entry: {}", entry.getName());
-        String className = formatPath(entry.getName()).substring(0, entry.getName().length() - 6);
-        addClass(className, Thread.currentThread().getContextClassLoader());
-      } else if (entry.isDirectory()) {
-        LOGGER.info("Dir entry: {}", entry.getName());
-        String subPackageName =
-            formatPath(entry.getName().substring(0, entry.getName().length() - 1));
-        scanClasses(jarInputStream, subPackageName);
-      }
-    }
+  protected void addClass(Class<?> clazz) {
+    classes.add(clazz);
   }
 
   public List<Class<?>> getClasses() {
     return classes;
   }
 
-  protected String formatPath(String path) {
-    return path.replace(".", "/");
-  }
-
-  protected void addClass(String className, ClassLoader loader) {
-    try {
-      Class<?> clazz = loader.loadClass(className);
-      classes.add(clazz);
-    } catch (ClassNotFoundException e) {
-      LOGGER.error("Failed to load class {}", className, e);
-    }
-  }
-
-  public List<Class<?>> getClassesWithAnnotation(Class<? extends Annotation> classAnotation) {
+  public List<Class<?>> getClassesWithAnnotation(Class<? extends Annotation> classAnnotation) {
     List<Class<?>> annotatedClasses = new ArrayList<>();
     for (Class<?> clazz : classes) {
       Annotation[] classAnnotations = clazz.getAnnotations();
       for (Annotation annotation : classAnnotations) {
-        if (annotation.annotationType() == classAnotation) {
+        if (annotation.annotationType() == classAnnotation) {
           annotatedClasses.add(clazz);
           break;
         }
       }
     }
     if (annotatedClasses.isEmpty()) {
-      LOGGER.warn("No class annotated with {}", classAnotation);
+      LOGGER.warn("No class annotated with {}", classAnnotation);
     }
     return annotatedClasses;
-  }
-
-  protected boolean checkJar() {
-    Class<?> clazz = ClassScanner.class;
-    ProtectionDomain protectionDomain = clazz.getProtectionDomain();
-    CodeSource codeSource = protectionDomain.getCodeSource();
-    LOGGER.info("Code source: {}", codeSource);
-    if (codeSource == null) {
-      return false;
-    } else {
-      URL location = codeSource.getLocation();
-      LOGGER.info("File location: {}", location);
-      return location.getPath().endsWith(".jar");
-    }
   }
 }
