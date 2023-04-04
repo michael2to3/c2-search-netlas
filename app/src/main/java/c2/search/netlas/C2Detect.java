6b6f0a049aa59c7caabcba3f30536859a687ea98
Michael <michael2ga3@gmail.com>
2023-04-04 17:25:08 +0300

    format code app

--- app/src/main/java/c2/search/netlas/C2Detect.java
+++ app/src/main/java/c2/search/netlas/C2Detect.java
@@ -50,13 +50,8 @@ public class C2Detect {
   }
 
   public void run(String[] args)
-      throws ClassNotFoundException,
-          IOException,
-          ParseException,
-          IllegalAccessException,
-          InstantiationException,
-          InvocationTargetException,
-          NoSuchMethodException,
+      throws ClassNotFoundException, IOException, ParseException, IllegalAccessException,
+          InstantiationException, InvocationTargetException, NoSuchMethodException,
           SecurityException {
     printWelcomMessage();
     setup(args);
@@ -75,11 +70,8 @@ public class C2Detect {
   }
 
   private void runChecker()
-      throws IllegalAccessException,
-          InstantiationException,
-          InvocationTargetException,
-          NoSuchMethodException,
-          SecurityException {
+      throws IllegalAccessException, InstantiationException, InvocationTargetException,
+          NoSuchMethodException, SecurityException {
     Results responses = checker.run();
     printResponses(responses, cmd.isVerbose());
   }
