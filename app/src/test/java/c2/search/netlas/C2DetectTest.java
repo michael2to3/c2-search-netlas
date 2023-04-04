127fee4a21b866180f1cf0b5eb85e1419d789d40
Michael <michael2ga3@gmail.com>
2023-04-04 13:19:17 +0300

    remove dublicate test

--- app/src/test/java/c2/search/netlas/C2DetectTest.java
+++ app/src/test/java/c2/search/netlas/C2DetectTest.java
@@ -24,35 +24,4 @@ public class C2DetectTest {
   public static void setup() {
     mockStatic(Config.class);
   }
-
-  @Test
-  public void testPrintHelp() {
-    String[] args = new String[] {"--help"};
-
-    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(outContent));
-
-    App.main(args);
-
-    assertTrue(outContent.toString().contains("-h"));
-  }
-
-  private Results getResults() {
-    Map<String, List<Response>> responses = new HashMap<>();
-    List<Response> moduleResponses1 = new ArrayList<>();
-    moduleResponses1.add(new Response(true, new Version("1.0", "0.1"), "Description 1", "Error"));
-    moduleResponses1.add(new Response(true, new Version("2.0", "0.2"), "Description 2", "Error"));
-    List<Response> moduleResponses2 = new ArrayList<>();
-    moduleResponses2.add(new Response(true, new Version("1.0", "0.1"), "Description 1", "Error"));
-    moduleResponses2.add(new Response(true, new Version("2.0", "0.2"), "Description 2", "Error"));
-    responses.put("Module 1", moduleResponses1);
-    responses.put("Module 2", moduleResponses2);
-
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    PrintStream printStream = new PrintStream(outputStream);
-    System.setOut(printStream);
-
-    Results results = new Results(responses);
-    return results;
-  }
 }
