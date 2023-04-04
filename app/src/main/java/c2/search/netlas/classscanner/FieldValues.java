2b86147267d40aa3cb07546e0cb3932ea6c4321b
Michael <michael2ga3@gmail.com>
2023-04-04 22:13:24 +0300

    enable logger guard + fix concurrent hash map

--- app/src/main/java/c2/search/netlas/classscanner/FieldValues.java
+++ app/src/main/java/c2/search/netlas/classscanner/FieldValues.java
@@ -1,14 +1,14 @@
 package c2.search.netlas.classscanner;
 
 import java.lang.reflect.Field;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FieldValues {
   private static final Logger LOGGER = LoggerFactory.getLogger(FieldValues.class);
-  private static final Map<Class<?>, Object> FIELDS = new HashMap<>();
+  private static final Map<Class<?>, Object> FIELDS = new ConcurrentHashMap<>();
 
   public FieldValues() {}
 
@@ -17,7 +17,9 @@ public class FieldValues {
   }
 
   public Object get(Field annotatedField) {
-    LOGGER.debug("Getting annotated field value for {}", annotatedField.getName());
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Getting annotated field value for {}", annotatedField.getName());
+    }
     return FIELDS.get(annotatedField.getType());
   }
 }
