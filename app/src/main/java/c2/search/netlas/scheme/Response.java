9fdf7b7247488af4707b9d3a67b677cc3b3ccf64
Michael <michael2ga3@gmail.com>
2023-04-05 15:59:06 +0300

    remove calculate in data class

--- app/src/main/java/c2/search/netlas/scheme/Response.java
+++ app/src/main/java/c2/search/netlas/scheme/Response.java
@@ -1,6 +1,5 @@
 package c2.search.netlas.scheme;
 
-import java.util.Objects;
 
 public class Response {
   private boolean success;
@@ -60,35 +59,5 @@ public class Response {
 
   public void setError(final String error) {
     this.error = error;
-  }
-
-  @Override
-  public String toString() {
-    return String.format(
-        "Response(version=%s, success=%s, description=%s, error=%s)",
-        version, success, description, error);
-  }
-
-  @Override
-  public boolean equals(final Object obj) {
-    boolean result;
-    if (this == obj) {
-      result = true;
-    } else if (obj == null || getClass() != obj.getClass()) {
-      result = false;
-    } else {
-      final Response response = (Response) obj;
-      result =
-          success == response.success
-              && version.equals(response.version)
-              && description.equals(response.description)
-              && error.equals(response.error);
-    }
-    return result;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(success, version, description, error);
   }
 }
