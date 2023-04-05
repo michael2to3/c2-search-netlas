6ff6401f73ca3bbe775d6d56d531878fc5b906d3
Michael <michael2ga3@gmail.com>
2023-04-05 16:13:06 +0300

    auto close input output stream socket

--- app/src/main/java/c2/search/netlas/target/metasploit/SocketConnection.java
+++ app/src/main/java/c2/search/netlas/target/metasploit/SocketConnection.java
@@ -5,35 +5,58 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SocketConnection implements AutoCloseable {
+  private static final Logger LOGGER = LoggerFactory.getLogger(SocketConnection.class);
   private static final int BUFFER_SIZE = 4096;
   private final String trigger;
   private final Socket socket;
+  private final InputStream inputStream;
+  private OutputStream outputStream;
 
   public SocketConnection(final Socket socket, final String trigger) {
     this.socket = socket;
     this.trigger = trigger;
+
+    InputStream input = null;
+    OutputStream output = null;
+    try {
+      input = socket.getInputStream();
+      output = socket.getOutputStream();
+    } catch (IOException e) {
+      LOGGER.error("Failed to get input/output streams", e);
+    }
+    this.inputStream = input;
+    this.outputStream = output;
   }
 
-  public String sendAndReceive() throws IOException {
+  public String sendAndReceive() {
     final String message = "echo " + trigger;
-    final OutputStream output = socket.getOutputStream();
-    String response;
-    try (InputStream input = socket.getInputStream()) {
+    String response = null;
 
-      output.write(message.getBytes());
-      output.flush();
+    try {
+      outputStream.write(message.getBytes());
+      outputStream.flush();
 
       final byte[] bresponse = new byte[BUFFER_SIZE];
-      final int responseLength = input.read(bresponse);
+      final int responseLength = inputStream.read(bresponse);
       response = new String(bresponse, 0, responseLength, StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      LOGGER.error("Failed to send message", e);
     }
     return response;
   }
 
   @Override
   public void close() throws IOException {
+    if (inputStream != null) {
+      inputStream.close();
+    }
+    if (outputStream != null) {
+      outputStream.close();
+    }
     socket.close();
   }
 }
