dcedc5799238d17a726238dde90ec08fe2a58fc9
Michael <michael2ga3@gmail.com>
2023-04-06 21:30:02 +0300

    add test cs jarm

--- app/src/main/java/c2/search/netlas/target/cobaltstrike/CobaltStrike.java
+++ app/src/main/java/c2/search/netlas/target/cobaltstrike/CobaltStrike.java
@@ -6,6 +6,7 @@ import c2.search.netlas.annotation.Wire;
 import c2.search.netlas.scheme.Host;
 import c2.search.netlas.scheme.Response;
 import c2.search.netlas.target.NetlasWrapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 @Detect(name = "CobaltStrike")
 public class CobaltStrike {
@@ -14,6 +15,14 @@ public class CobaltStrike {
 
   @Test
   public Response testJarm() {
-    return new Response(false);
+    final String jarm = "2ad2ad16d2ad2ad00042d42d00042ddb04deffa1705e2edc44cae1ed24a4da";
+    String tjarm;
+    try {
+      tjarm = netlasWrapper.getJarm();
+    } catch (JsonProcessingException e) {
+      tjarm = null;
+    }
+
+    return new Response(jarm.equals(tjarm));
   }
 }
