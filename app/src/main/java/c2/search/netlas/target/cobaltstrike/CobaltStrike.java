5df187586e7a963d3f14e0f82cf1074f1ae20ea9
Michael <michael2ga3@gmail.com>
2023-04-08 11:56:26 +0300

    add support cert

--- app/src/main/java/c2/search/netlas/target/cobaltstrike/CobaltStrike.java
+++ app/src/main/java/c2/search/netlas/target/cobaltstrike/CobaltStrike.java
@@ -6,6 +6,9 @@ import c2.search.netlas.annotation.Wire;
 import c2.search.netlas.scheme.Host;
 import c2.search.netlas.target.NetlasWrapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import java.util.Arrays;
+import java.util.List;
 
 @Detect(name = "CobaltStrike")
 public class CobaltStrike {
@@ -23,5 +26,115 @@ public class CobaltStrike {
     }
 
     return jarm.equals(tjarm);
+  }
+
+  @Test
+  public boolean defaultCertFieldTeamServer() throws JsonMappingException, JsonProcessingException {
+    // * subject: C=US; ST=Washington; L=Redmond; O=Microsoft Corporation;
+    // OU=Microsoft Corporation; CN=Outlook.live.com
+    // * issuer: C=US; ST=Washington; L=Redmond; O=Microsoft Corporation;
+    // OU=Microsoft Corporation; CN=Outlook.live.com
+    final String country = "US";
+    final String state = "Washington";
+    final String city = "Redmond";
+    final String organization = "Microsoft Corporation";
+    final String organizationUnit = "Microsoft Corporation";
+    final String commonName = "Outlook.live.com";
+
+    final List<String> subjectCountry = netlasWrapper.getCertSubjectCountry();
+    final List<String> subjectState = netlasWrapper.getCertSubjectProvince();
+    final List<String> subjectCity = netlasWrapper.getCertSubjectLocality();
+    final List<String> subjectOrganization = netlasWrapper.getCertSubjectOrganization();
+    final List<String> subjectOrganizationUnit = netlasWrapper.getCertSubjectOrganizationUnit();
+    final List<String> subjectCommonName = netlasWrapper.getCertSubjectCommonName();
+
+    final List<String> issuerCountry = netlasWrapper.getCertIssuerCountry();
+    final List<String> issuerState = netlasWrapper.getCertIssuerProvince();
+    final List<String> issuerCity = netlasWrapper.getCertIssuerLocality();
+    final List<String> issuerOrganization = netlasWrapper.getCertIssuerOrganization();
+    final List<String> issuerOrganizationUnit = netlasWrapper.getCertIssuerOrganizationUnit();
+    final List<String> issuerCommonName = netlasWrapper.getCertIssuerCommonName();
+
+    final List<List<String>> subject =
+        Arrays.asList(
+            subjectCountry,
+            subjectState,
+            subjectCity,
+            subjectOrganization,
+            subjectOrganizationUnit,
+            subjectCommonName);
+    final List<List<String>> issuer =
+        Arrays.asList(
+            issuerCountry,
+            issuerState,
+            issuerCity,
+            issuerOrganization,
+            issuerOrganizationUnit,
+            issuerCommonName);
+
+    return allEqual(subject, country, state, city, organization, organizationUnit, commonName)
+        && allEqual(issuer, country, state, city, organization, organizationUnit, commonName);
+  }
+
+  @Test
+  public boolean defaultCertFieldListener() throws JsonMappingException, JsonProcessingException {
+    // * subject: C=; ST=; L=; O=; OU=; CN=
+    // * issuer: C=; ST=; L=; O=; OU=; CN=
+    final String country = "";
+    final String state = "";
+    final String city = "";
+    final String organization = "";
+    final String organizationUnit = "";
+    final String commonName = "";
+
+    final List<String> subjectCountry = netlasWrapper.getCertSubjectCountry();
+    final List<String> subjectState = netlasWrapper.getCertSubjectProvince();
+    final List<String> subjectCity = netlasWrapper.getCertSubjectLocality();
+    final List<String> subjectOrganization = netlasWrapper.getCertSubjectOrganization();
+    final List<String> subjectOrganizationUnit = netlasWrapper.getCertSubjectOrganizationUnit();
+    final List<String> subjectCommonName = netlasWrapper.getCertSubjectCommonName();
+
+    final List<String> issuerCountry = netlasWrapper.getCertIssuerCountry();
+    final List<String> issuerState = netlasWrapper.getCertIssuerProvince();
+    final List<String> issuerCity = netlasWrapper.getCertIssuerLocality();
+    final List<String> issuerOrganization = netlasWrapper.getCertIssuerOrganization();
+    final List<String> issuerOrganizationUnit = netlasWrapper.getCertIssuerOrganizationUnit();
+    final List<String> issuerCommonName = netlasWrapper.getCertIssuerCommonName();
+
+    final List<List<String>> subject =
+        Arrays.asList(
+            subjectCountry,
+            subjectState,
+            subjectCity,
+            subjectOrganization,
+            subjectOrganizationUnit,
+            subjectCommonName);
+    final List<List<String>> issuer =
+        Arrays.asList(
+            issuerCountry,
+            issuerState,
+            issuerCity,
+            issuerOrganization,
+            issuerOrganizationUnit,
+            issuerCommonName);
+
+    return allEqual(subject, country, state, city, organization, organizationUnit, commonName)
+        && allEqual(issuer, country, state, city, organization, organizationUnit, commonName);
+  }
+
+  private boolean allEqual(List<List<String>> lists, String... expectedValues) {
+    boolean result;
+    if (lists.size() != expectedValues.length) {
+      result = false;
+    } else {
+      for (int i = 0; i < lists.size(); i++) {
+        if (!lists.get(i).contains(expectedValues[i])) {
+          result = false;
+          break;
+        }
+      }
+      result = true;
+    }
+    return result;
   }
 }
