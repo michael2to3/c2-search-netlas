552168c5e5008966c79f1c4559a68c2fe0088215
Michael <michael2ga3@gmail.com>
2023-04-09 00:31:33 +0300

    split large class

--- app/src/main/java/c2/search/netlas/target/cobaltstrike/CobaltStrike.java
+++ app/src/main/java/c2/search/netlas/target/cobaltstrike/CobaltStrike.java
@@ -8,10 +8,6 @@ import c2.search.netlas.target.NetlasWrapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.Socket;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import org.slf4j.Logger;
@@ -43,55 +39,13 @@ public class CobaltStrike {
       "Microsoft Corporation",
       "Outlook.live.com",
     };
-    return verifyDefaultCertFields(fields);
+    return Utils.verifyDefaultCertFields(netlasWrapper, fields);
   }
 
   @Test
   public boolean defaultCertFieldListener() throws JsonMappingException, JsonProcessingException {
     final String[] fields = {"", "", "", "", "", ""};
-    return verifyDefaultCertFields(fields);
-  }
-
-  private boolean verifyDefaultCertFields(final String[] fields)
-      throws JsonMappingException, JsonProcessingException {
-    final List<String> subCountry = netlasWrapper.getCertSubjectCountry();
-    final List<String> subState = netlasWrapper.getCertSubjectProvince();
-    final List<String> subCity = netlasWrapper.getCertSubjectLocality();
-    final List<String> subOrg = netlasWrapper.getCertSubjectOrganization();
-    final List<String> subOrgUnit = netlasWrapper.getCertSubjectOrganizationUnit();
-    final List<String> subCommonName = netlasWrapper.getCertSubjectCommonName();
-
-    final List<String> issCountry = netlasWrapper.getCertIssuerCountry();
-    final List<String> issState = netlasWrapper.getCertIssuerProvince();
-    final List<String> issCity = netlasWrapper.getCertIssuerLocality();
-    final List<String> issOrg = netlasWrapper.getCertIssuerOrganization();
-    final List<String> issOrgUnit = netlasWrapper.getCertIssuerOrganizationUnit();
-    final List<String> issCommonName = netlasWrapper.getCertIssuerCommonName();
-
-    final List<List<String>> subject =
-        Arrays.asList(subCountry, subState, subCity, subOrg, subOrgUnit, subCommonName);
-    final List<List<String>> issuer =
-        Arrays.asList(issCountry, issState, issCity, issOrg, issOrgUnit, issCommonName);
-
-    boolean result = true;
-    for (int i = 0; i < fields.length; i++) {
-      if (!fields[i].equals("")) {
-        if (!allEqual(subject.get(i), fields[i])) {
-          result = false;
-          break;
-        }
-        if (!allEqual(issuer.get(i), fields[i])) {
-          result = false;
-          break;
-        }
-      }
-    }
-
-    return result;
-  }
-
-  private boolean allEqual(final List<String> list, final String value) {
-    return list.stream().allMatch(s -> s.equals(value));
+    return Utils.verifyDefaultCertFields(netlasWrapper, fields);
   }
 
   @Test
@@ -117,7 +71,7 @@ public class CobaltStrike {
     final int max = 150;
     boolean result = false;
     try {
-      final int len = getDataLength();
+      final int len = Utils.getDataLength(host);
       result = len >= min && len <= max;
     } catch (IOException e) {
       if (LOGGER.isWarnEnabled()) {
@@ -132,43 +86,12 @@ public class CobaltStrike {
     boolean result = false;
     final List<String> paths = Arrays.asList("/accept.php", "/pixel");
     for (final String path : paths) {
-      final int statusWithUA = sendHttpRequest("random", path);
-      final int statusWithoutUA = sendHttpRequest(null, path);
+      final int statusWithUA = Utils.sendHttpRequest(host, "random", path);
+      final int statusWithoutUA = Utils.sendHttpRequest(host, null, path);
       final int accept = 200;
       final int reject = 404;
       result = statusWithUA == accept && statusWithoutUA == reject;
     }
     return result;
-  }
-
-  private int getDataLength() throws IOException {
-    final int chunk = 1024;
-    try (Socket socket = new Socket(host.getTarget(), host.getPort());
-        InputStream inputStream = socket.getInputStream()) {
-      final byte[] buffer = new byte[chunk];
-      int len;
-      int totalLen = 0;
-      while (true) {
-        len = inputStream.read(buffer);
-        if (len == -1) {
-          break;
-        }
-        totalLen += len;
-      }
-      return totalLen;
-    }
-  }
-
-  public int sendHttpRequest(final String ua, final String path) throws IOException {
-    final String urlString = "http://" + host.getTarget() + ":" + host.getPort() + path;
-    final URL url = new URL(urlString);
-    final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-    connection.setRequestMethod("GET");
-    if (!ua.isEmpty() || ua == null) {
-      connection.setRequestProperty("User-Agent", "random");
-    }
-    final int statusCode = connection.getResponseCode();
-    connection.disconnect();
-    return statusCode;
   }
 }
