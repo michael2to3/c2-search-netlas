c4d26b80cccc0677f24ad8333cf949cb5aa45851
Michael <michael2ga3@gmail.com>
2023-04-08 16:28:58 +0300

    add check bind shell and ua

--- app/src/main/java/c2/search/netlas/target/cobaltstrike/CobaltStrike.java
+++ app/src/main/java/c2/search/netlas/target/cobaltstrike/CobaltStrike.java
@@ -7,11 +7,19 @@ import c2.search.netlas.scheme.Host;
 import c2.search.netlas.target.NetlasWrapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Detect(name = "CobaltStrike")
 public class CobaltStrike {
+  private static final Logger LOGGER = LoggerFactory.getLogger(CobaltStrike.class);
   @Wire private Host host;
   @Wire private NetlasWrapper netlasWrapper;
 
@@ -133,6 +141,42 @@ public class CobaltStrike {
     return emptyServer && len && contentType;
   }
 
+  @Test
+  public boolean defaultPort() throws JsonMappingException, JsonProcessingException {
+    List<Integer> ports = Arrays.asList(41337, 1337, 4444, 2222, 50050);
+    return ports.contains(host.getPort());
+  }
+
+  @Test(extern = true)
+  public boolean lenIdBindShell() throws JsonMappingException, JsonProcessingException {
+    final int min = 100;
+    final int max = 150;
+    boolean result = false;
+    try {
+      final int len = getDataLength();
+      result = len >= min && len <= max;
+    } catch (IOException e) {
+      if (LOGGER.isWarnEnabled()) {
+        LOGGER.warn("Failed to get data length", e);
+      }
+    }
+    return result;
+  }
+
+  @Test(extern = true)
+  public boolean checkUA() throws IOException {
+    boolean result = false;
+    List<String> paths = Arrays.asList("/accept.php", "/pixel");
+    for (String path : paths) {
+      int statusWithUA = sendHttpRequest("random", path);
+      int statusWithoutUA = sendHttpRequest(null, path);
+      int accept = 200;
+      int reject = 404;
+      result = statusWithUA == accept && statusWithoutUA == reject;
+    }
+    return result;
+  }
+
   private boolean allEqual(List<List<String>> lists, String... expectedValues) {
     boolean result;
     if (lists.size() != expectedValues.length) {
@@ -147,5 +191,43 @@ public class CobaltStrike {
       result = true;
     }
     return result;
+  }
+
+  private int getDataLength() throws IOException {
+    Socket socket = null;
+    InputStream inputStream = null;
+    try {
+      socket = new Socket(host.getTarget(), host.getPort());
+      inputStream = socket.getInputStream();
+      byte[] buffer = new byte[1024];
+      int len;
+      int totalLen = 0;
+      while ((len = inputStream.read(buffer)) != -1) {
+        totalLen += len;
+      }
+      return totalLen;
+    } catch (IOException e) {
+      throw e;
+    } finally {
+      if (inputStream != null) {
+        inputStream.close();
+      }
+      if (socket != null) {
+        socket.close();
+      }
+    }
+  }
+
+  public int sendHttpRequest(String ua, String path) throws IOException {
+    String urlString = "http://" + host.getTarget() + ":" + host.getPort() + path;
+    URL url = new URL(urlString);
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("GET");
+    if (!ua.isEmpty() || ua == null) {
+      connection.setRequestProperty("User-Agent", "random");
+    }
+    int statusCode = connection.getResponseCode();
+    connection.disconnect();
+    return statusCode;
   }
 }
