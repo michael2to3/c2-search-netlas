973a340ae0bb293061ac17de4e6957635f3ff66a
Michael <michael2ga3@gmail.com>
2023-04-12 15:13:56 +0300

    refactoring methods utils

--- app/src/main/java/c2/search/netlas/target/villain/Utils.java
+++ app/src/main/java/c2/search/netlas/target/villain/Utils.java
@@ -22,8 +22,11 @@ import javax.net.ssl.X509TrustManager;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 final class Utils {
+  private static final Logger LOGGER = LoggerFactory.getLogger(Utils.class);
 
   private Utils() {}
 
@@ -70,7 +73,11 @@ final class Utils {
       final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
       final StringBuilder response = new StringBuilder();
       String line;
-      while ((line = in.readLine()) != null) {
+      while (true) {
+        line = in.readLine();
+        if (line == null) {
+          break;
+        }
         response.append(line);
       }
       return response.toString();
@@ -81,23 +88,28 @@ final class Utils {
       throws IOException, NoSuchAlgorithmException, KeyManagementException {
     String url = "https://" + host.getTarget() + ":" + host.getPort() + path;
     final OkHttpClient client = getUnsafeOkHttpClient();
+    String responseString = null;
     try {
       final Request request = new Request.Builder().url(url).build();
       final Response response = client.newCall(request).execute();
-      return response.body().string();
+      responseString = response.body().string();
     } catch (final IOException e) {
       url = "http://" + host.getTarget() + ":" + host.getPort() + path;
       try {
         final Request request = new Request.Builder().url(url).build();
         final Response response = client.newCall(request).execute();
-        return response.body().string();
-      } catch (final IOException e1) {
-        return null;
+        responseString = response.body().string();
+      } catch (final IOException ex) {
+        if (LOGGER.isWarnEnabled()) {
+          LOGGER.warn("Failed to connect to " + url, ex);
+        }
       }
     }
+    return responseString;
   }
 
   private static OkHttpClient getUnsafeOkHttpClient() {
+    OkHttpClient okHttpClient = null;
     try {
       final TrustManager[] trustAllCerts =
           new TrustManager[] {
@@ -133,10 +145,12 @@ final class Utils {
             }
           });
 
-      final OkHttpClient okHttpClient = builder.build();
-      return okHttpClient;
-    } catch (final Exception e) {
-      throw new RuntimeException(e);
+      okHttpClient = builder.build();
+    } catch (NoSuchAlgorithmException | KeyManagementException e) {
+      if (LOGGER.isWarnEnabled()) {
+        LOGGER.warn("Failed to create OkHttpClient", e);
+      }
     }
+    return okHttpClient;
   }
 }
