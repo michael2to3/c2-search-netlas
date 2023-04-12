7cf1323a388a75a0d3fad33aed70a600677571e1
Michael <michael2ga3@gmail.com>
2023-04-12 17:19:11 +0300

    create complex test phoenix

---
+++ app/src/main/java/c2/search/netlas/target/phoenix/NetworkUtils.java
@@ -0,0 +1,61 @@
+package c2.search.netlas.target.phoenix;
+
+import c2.search.netlas.scheme.Host;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class NetworkUtils {
+  private static final TrustManager[] trustAllCerts = {
+    new X509TrustManager() {
+      public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+        return new java.security.cert.X509Certificate[] {};
+      }
+
+      public void checkClientTrusted(final java.security.cert.X509Certificate[] chain, final String authType)
+          throws java.security.cert.CertificateException {}
+
+      public void checkServerTrusted(final java.security.cert.X509Certificate[] chain, final String authType)
+          throws java.security.cert.CertificateException {}
+    },
+  };
+
+  private static OkHttpClient getUnsafeOkHttpClient()
+      throws NoSuchAlgorithmException, KeyManagementException {
+    final SSLContext sslContext = SSLContext.getInstance("SSL");
+    sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+    final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+    builder.sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0]);
+    builder.hostnameVerifier((hostname, session) -> true);
+    return builder.build();
+  }
+
+  public static int getStatus(final Host host, final String path)
+      throws IOException, KeyManagementException, NoSuchAlgorithmException {
+    int statusCode;
+
+    String url = "https://" + host.getTarget() + ":" + host.getPort() + path;
+    OkHttpClient client = getUnsafeOkHttpClient();
+    Request request = new Request.Builder().url(url).build();
+    try (Response response = client.newCall(request).execute()) {
+      statusCode = response.code();
+    } catch (final IOException ex) {
+      url = "http://" + host.getTarget() + ":" + host.getPort() + path;
+      client = new OkHttpClient();
+      request = new Request.Builder().url(url).build();
+      try (Response response = client.newCall(request).execute()) {
+        statusCode = response.code();
+      } catch (IOException e) {
+        throw e;
+      }
+    }
+
+    return statusCode;
+  }
+}
--- app/src/main/java/c2/search/netlas/target/phoenix/Phoenix.java
+++ app/src/main/java/c2/search/netlas/target/phoenix/Phoenix.java
@@ -7,6 +7,11 @@ import c2.search.netlas.scheme.Host;
 import c2.search.netlas.target.NetlasWrapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
 
 @Detect(name = "Phoenix")
 public class Phoenix {
@@ -20,5 +25,54 @@ public class Phoenix {
     final String jarm = "2ad2ad0002ad2ad22c42d42d000000faabb8fd156aa8b4d8a37853e1063261";
     final String tjarm = netlasWrapper.getJarm();
     return jarm.equals(tjarm);
+  }
+
+  @Test
+  public boolean checkFieldCert() throws JsonMappingException, JsonProcessingException {
+    final String subCountry = "US";
+    final String issCountry = "US";
+    final List<String> rsubCountry = netlasWrapper.getCertSubjectCountry();
+    final List<String> rissCountry = netlasWrapper.getCertIssuerCountry();
+    return rsubCountry.contains(subCountry) && rissCountry.contains(issCountry);
+  }
+
+  @Test
+  public boolean checkDefaultBodyResponse() throws JsonMappingException, JsonProcessingException {
+    final List<String> body =
+        Arrays.asList(
+            "bdd3acd38b235f3e79f97834c1bada34fe87489f5cc3c530dab5bc47404e0a87",
+            "e9639e3c4681ce85f852fbac48e2eeee5ba51296dbfec57c200d59b76237ab80");
+    final String rbody = netlasWrapper.getBodyAsSha256();
+    return body.contains(rbody);
+  }
+
+  @Test
+  public boolean checkListenerResponse()
+      throws KeyManagementException, NoSuchAlgorithmException, IOException {
+    String[] paths = {"download/history.csv", "download/users.csv", "static/dump.sql"};
+    final int accessDenied = 405;
+    boolean result = true;
+    for (String path : paths) {
+      result = NetworkUtils.getStatus(host, path) == accessDenied;
+      if (result == false) {
+        break;
+      }
+    }
+    return result;
+  }
+
+  @Test
+  public boolean headerServer() throws JsonMappingException, JsonProcessingException {
+    final List<String> servers = netlasWrapper.getServers();
+    final List<String> types = netlasWrapper.getContentType();
+    final String baseType = "text/html; charset=utf-8";
+    final String base = "Werkzeug";
+    boolean result = false;
+    for (final String server : servers) {
+      if (server.contains(base)) {
+        result = true;
+      }
+    }
+    return result && types.contains(baseType);
   }
 }
