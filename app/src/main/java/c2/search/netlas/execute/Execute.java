9d21597fdee35a86c09a74c0e917ceb860ad2271
Michael <michael2ga3@gmail.com>
2023-04-13 12:37:52 +0300

    move and split checker

---
+++ app/src/main/java/c2/search/netlas/execute/Execute.java
@@ -0,0 +1,160 @@
+package c2.search.netlas.execute;
+
+import c2.search.netlas.annotation.BeforeAll;
+import c2.search.netlas.annotation.Detect;
+import c2.search.netlas.annotation.Test;
+import c2.search.netlas.classscanner.ClassScanner;
+import c2.search.netlas.classscanner.FieldValues;
+import c2.search.netlas.scheme.Response;
+import c2.search.netlas.scheme.ResponseBuilder;
+import c2.search.netlas.scheme.Results;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Execute {
+  private static final Logger LOGGER = LoggerFactory.getLogger(Execute.class);
+  private static final int TIMEOUT_SECOND = 10;
+  private static final String TARGET_CLASS_NAME = "c2.search.netlas.target";
+  private ClassScanner classScanner;
+  private final Factory factory;
+
+  public Execute(FieldValues fields) {
+    this.classScanner = new ClassScanner(TARGET_CLASS_NAME);
+    this.factory = new Factory(fields);
+  }
+
+  public Results run() {
+    ExecutorService executor = Executors.newFixedThreadPool(1);
+    List<Future<Results>> futures = new ArrayList<>();
+    for (Class<?> clazz : classScanner.getClassesWithAnnotation(Detect.class)) {
+      Future<Results> future = executor.submit(
+          () -> {
+            Object instance = factory.createInstance(clazz);
+            invokeBeforeAllMethods(instance);
+            List<Response> responses = invokeTestMethods(instance);
+            Results results = new Results();
+            results.addResponse(getNameOfClass(clazz), responses);
+            return results;
+          });
+      futures.add(future);
+    }
+    Results results = new Results();
+    for (var future : futures) {
+      try {
+        results.merge(future.get(TIMEOUT_SECOND, TimeUnit.SECONDS));
+      } catch (InterruptedException | ExecutionException e) {
+        LOGGER.error("Error while running tests", e);
+      } catch (TimeoutException e) {
+        LOGGER.error("Timeout while running tests", e);
+      }
+    }
+    executor.shutdown();
+    return results;
+  }
+
+  private List<Method> getBeforeAllMethods(final Class<?> clazz) {
+    final List<Method> beforeAllMethods = new ArrayList<>();
+    for (final Method method : clazz.getMethods()) {
+      if (method.isAnnotationPresent(BeforeAll.class)) {
+        beforeAllMethods.add(method);
+      }
+    }
+    return beforeAllMethods;
+  }
+
+  private void invokeBeforeAllMethods(Object instance) {
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Invoking beforeAll methods of {}", instance.getClass().getName());
+    }
+    final List<Method> beforeAllMethods = getBeforeAllMethods(instance.getClass());
+    for (final Method method : beforeAllMethods) {
+      try {
+        method.invoke(instance);
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        handleInvocationError(method, instance, e);
+      }
+    }
+  }
+
+  private List<Response> invokeTestMethods(Object instance) {
+    final List<Response> responses = new ArrayList<>();
+    for (final Method method : getTestMethods(instance.getClass())) {
+      try {
+        responses.add(invokeTestMethod(method, instance));
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        handleInvocationError(method, instance, e);
+      }
+    }
+
+    return responses;
+  }
+
+  private List<Method> getTestMethods(final Class<?> clazz) {
+    final List<Method> testMethods = new ArrayList<>();
+    for (final Method method : clazz.getMethods()) {
+      if (method.isAnnotationPresent(Test.class)) {
+        testMethods.add(method);
+      }
+    }
+    return testMethods;
+  }
+
+  private String getDescriptionOfTestMethod(final Method method) {
+    String description = method.getAnnotation(Test.class).description();
+    if (description == null || description.isEmpty()) {
+      description = method.getName();
+    }
+    return description;
+  }
+
+  private Response invokeTestMethod(final Method method, final Object instant)
+      throws IllegalAccessException, InvocationTargetException {
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Invoking test methods of {}", instant.getClass().getName());
+    }
+    Response response = null;
+    final String desc = getDescriptionOfTestMethod(method);
+    if (method.getReturnType() == boolean.class) {
+      final boolean success = (boolean) method.invoke(instant);
+      response = new ResponseBuilder().setSuccess(success).setDescription(desc).build();
+    } else if (method.getReturnType() == Response.class) {
+      response = (Response) method.invoke(instant);
+      response.setDescription(desc);
+    } else {
+      if (LOGGER.isErrorEnabled()) {
+        LOGGER.error("Unsupported return type of test method {}", method.getName());
+      }
+    }
+    return response;
+  }
+
+  private void handleInvocationError(
+      final Method method, final Object instant, final Exception exception) {
+    if (LOGGER.isInfoEnabled()) {
+      LOGGER.info(
+          "Error invoking test method {} on {} - {}",
+          method.getName(),
+          instant.getClass().getName(),
+          exception.getMessage());
+    }
+  }
+
+  private String getNameOfClass(final Class<?> clazz) {
+    final Detect detect = clazz.getAnnotation(Detect.class);
+    String nameOfDetect = detect.name();
+    if (nameOfDetect == null || nameOfDetect.isEmpty()) {
+      nameOfDetect = clazz.getName();
+    }
+    return nameOfDetect;
+  }
+}
---
+++ app/src/main/java/c2/search/netlas/execute/Factory.java
@@ -0,0 +1,34 @@
+package c2.search.netlas.execute;
+import java.lang.reflect.InvocationTargetException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import c2.search.netlas.classscanner.FieldValues;
+
+class Factory {
+  private static final Logger LOGGER = LoggerFactory.getLogger(Execute.class);
+  private final FieldValues fields;
+
+  public Factory(final FieldValues fields) {
+    this.fields = fields;
+  }
+
+  public Object createInstance(final Class<?> clazz) {
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Instantiating {}", clazz.getName());
+    }
+    Object instant = null;
+    try {
+      instant = clazz.getDeclaredConstructor().newInstance();
+    } catch (InstantiationException
+        | IllegalAccessException
+        | InvocationTargetException
+        | NoSuchMethodException e) {
+      if (LOGGER.isErrorEnabled()) {
+        LOGGER.error("Failed to instantiate {}", clazz.getName(), e);
+      }
+    }
+    return instant;
+  }
+}
