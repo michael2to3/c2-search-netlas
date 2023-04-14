a54080c982fdf99428cb4130d03a9ecd86a465a6
Michael <michael2ga3@gmail.com>
2023-04-14 21:02:57 +0300

    create default path to contain all config file

--- app/src/main/java/c2/search/netlas/cli/Config.java
+++ app/src/main/java/c2/search/netlas/cli/Config.java
@@ -1,10 +1,10 @@
 package c2.search.netlas.cli;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Locale;
 import java.util.Properties;
@@ -13,24 +13,18 @@ import org.slf4j.LoggerFactory;
 
 public class Config {
   private static final Logger LOGGER = LoggerFactory.getLogger(Config.class);
-  private final String fileName;
-  private final Properties props;
+  final Path parentDir = Paths.get(System.getProperty("user.home"), ".c2detect");
+  private final Path filePath;
+  private final Properties properties;
 
-  public Config(final String fileName) {
-    this.fileName = fileName;
-    this.props = new Properties();
-
-    try (InputStream input = Files.newInputStream(Paths.get(fileName))) {
-      props.load(input);
-    } catch (final FileNotFoundException e) {
-      LOGGER.error("Config file not found: {}", fileName, e);
-    } catch (final IOException e) {
-      LOGGER.error("Error reading config file: {}", fileName, e);
-    }
+  public Config(final String filePath) {
+    this.filePath = parentDir.resolve(Paths.get(filePath));
+    this.properties = new Properties();
+    loadProperties();
   }
 
   public String get(final String key) {
-    String value = props.getProperty(key);
+    String value = properties.getProperty(key);
     if (value == null) {
       value = System.getenv(key);
     }
@@ -42,11 +36,43 @@ public class Config {
   }
 
   public void save(final String key, final String value) {
-    props.setProperty(key, value);
-    try (OutputStream output = Files.newOutputStream(Paths.get(fileName))) {
-      props.store(output, null);
+    properties.setProperty(key, value);
+    try (OutputStream output = Files.newOutputStream(filePath)) {
+      properties.store(output, null);
     } catch (final IOException e) {
-      LOGGER.error("Error writing config file: {}", fileName, e);
+      LOGGER.error("Error writing config file: {}", filePath, e);
+    }
+  }
+
+  private void loadProperties() {
+    if (!Files.exists(filePath)) {
+      LOGGER.warn("Config file not found: {}", filePath);
+      createFile();
+    }
+    try (InputStream input = Files.newInputStream(filePath)) {
+      properties.load(input);
+    } catch (final IOException e) {
+      LOGGER.error("Error reading config file: {}", filePath, e);
+    }
+  }
+
+  private void createFile() {
+    LOGGER.error("Config file not found: {}", filePath);
+    if (!Files.exists(parentDir)) {
+      LOGGER.debug("Creating config directory: {}", parentDir);
+      try {
+        Files.createDirectories(parentDir);
+      } catch (final IOException e) {
+        LOGGER.error("Error creating config directory: {}", parentDir, e);
+        return;
+      }
+    }
+
+    LOGGER.debug("Creating config file: {}", filePath);
+    try {
+      Files.createFile(filePath);
+    } catch (final IOException e) {
+      LOGGER.error("Error creating config file: {}", filePath, e);
     }
   }
 }
