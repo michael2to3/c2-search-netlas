b005524b37a2011f679bbdc90063e1b086f68823
Michael <michael2ga3@gmail.com>
2023-04-27 19:00:48 +0300

    create netlas cache

---
+++ app/src/main/java/c2/search/netlas/NetlasCache.java
@@ -0,0 +1,49 @@
+package c2.search.netlas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import netlas.java.Netlas;
+import netlas.java.exception.NetlasRequestException;
+import netlas.java.scheme.Response;
+
+public class NetlasCache {
+  private static NetlasCache instance = null;
+  private final Netlas netlas;
+  private final Map<String, Object> cache;
+
+  private NetlasCache(String apiKey) {
+    this.netlas = Netlas.newBuilder().setApiKey(apiKey).build();
+    this.cache = new HashMap<>();
+  }
+
+  public static NetlasCache getInstance(String apiKey) {
+    if (instance == null) {
+      instance = new NetlasCache(apiKey);
+    }
+    return instance;
+  }
+
+  public Response response(
+      String query, int page, String indices, String fields, boolean excludeFields)
+      throws NetlasRequestException {
+    String cacheKey = buildCacheKey("response", query, page, indices, fields, excludeFields);
+    var response = (Response) cache.get(cacheKey);
+    if (response == null) {
+      response = fetchResponse(query, page, indices, fields, excludeFields);
+      cache.put(cacheKey, response);
+    }
+    return response;
+  }
+
+  public netlas.java.scheme.Response fetchResponse(
+      String query, int page, String indices, String fields, boolean excludeFields)
+      throws NetlasRequestException {
+    return netlas.response(query, page, indices, fields, excludeFields);
+  }
+
+  private String buildCacheKey(
+      String method, String query, int page, String indices, String fields, boolean excludeFields) {
+    return method + "|" + query + "|" + page + "|" + indices + "|" + fields + "|" + excludeFields;
+  }
+}
