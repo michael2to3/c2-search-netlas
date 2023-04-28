e16e54a3c031432e54d191eccddd751e6d0be41b
Michael <michael2ga3@gmail.com>
2023-04-28 09:03:52 +0300

    add check valid submask

--- app/src/main/java/c2/search/netlas/broadcast/NormalizeRangeHost.java
+++ app/src/main/java/c2/search/netlas/broadcast/NormalizeRangeHost.java
@@ -15,28 +15,35 @@ public class NormalizeRangeHost {
 
   public String normalize() throws IpRangeFormatException {
     String normalizedRange = null;
-    Pattern pattern =
-        Pattern.compile(
-            "(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\\s*[-TO,]\\s*(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})");
+    Pattern pattern = Pattern.compile(
+        "(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\\s*[-TO,]\\s*(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})");
+    Pattern subnetPattern = Pattern.compile("(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})/(\\d{1,2})");
     Matcher matcher = pattern.matcher(range);
+    Matcher subnetMatcher = subnetPattern.matcher(range);
 
     if (matcher.find()) {
       String startIP = matcher.group(1);
       String endIP = matcher.group(2);
 
-      try {
-        InetAddress startAddress = InetAddress.getByName(startIP);
-        InetAddress endAddress = InetAddress.getByName(endIP);
+      if (!isValidIPAddress(startIP) || !isValidIPAddress(endIP)) {
+        throw new IpRangeFormatException("Invalid IP range format");
+      }
 
-        ByteBuffer startAddressBuffer = ByteBuffer.wrap(startAddress.getAddress());
-        ByteBuffer endAddressBuffer = ByteBuffer.wrap(endAddress.getAddress());
+      normalizedRange = processIpRange(matcher);
+    } else if (subnetMatcher.find()) {
+      subnetPattern = Pattern.compile("(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\\/(\\d{1,2})");
+      subnetMatcher = subnetPattern.matcher(range);
 
-        if (startAddressBuffer.compareTo(endAddressBuffer) <= 0) {
-          normalizedRange = "[" + startIP + " TO " + endIP + "]";
+      if (subnetMatcher.find()) {
+        String ipAddress = subnetMatcher.group(1);
+        String subnetMask = subnetMatcher.group(2);
+
+        if (isValidIPAddress(ipAddress) && isValidSubnetMask(subnetMask)) {
+          return "\"" + ipAddress + "/" + subnetMask + "\"";
         } else {
-          throw new IpRangeFormatException("Invalid IP range");
+          throw new IpRangeFormatException("Invalid IP range format");
         }
-      } catch (UnknownHostException e) {
+      } else {
         throw new IpRangeFormatException("Invalid IP range format");
       }
     } else {
@@ -44,5 +51,57 @@ public class NormalizeRangeHost {
     }
 
     return normalizedRange;
+  }
+
+  private String processIpRange(Matcher matcher) throws IpRangeFormatException {
+    String startIP = matcher.group(1);
+    String endIP = matcher.group(2);
+
+    try {
+      InetAddress startAddress = InetAddress.getByName(startIP);
+      InetAddress endAddress = InetAddress.getByName(endIP);
+
+      ByteBuffer startAddressBuffer = ByteBuffer.wrap(startAddress.getAddress());
+      ByteBuffer endAddressBuffer = ByteBuffer.wrap(endAddress.getAddress());
+
+      if (startAddressBuffer.compareTo(endAddressBuffer) <= 0) {
+        return "[" + startIP + " TO " + endIP + "]";
+      } else {
+        throw new IpRangeFormatException("Invalid IP range");
+      }
+    } catch (UnknownHostException e) {
+      throw new IpRangeFormatException("Invalid IP range format");
+    }
+  }
+
+  private boolean isValidIPAddress(String ipAddress) {
+    String[] octets = ipAddress.split("\\.");
+
+    if (octets.length != 4) {
+      return false;
+    }
+
+    for (String octet : octets) {
+      try {
+        int num = Integer.parseInt(octet);
+
+        if (num < 0 || num > 255) {
+          return false;
+        }
+      } catch (NumberFormatException e) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private boolean isValidSubnetMask(String subnetMask) {
+    try {
+      int mask = Integer.parseInt(subnetMask);
+      return mask >= 0 && mask <= 32;
+    } catch (NumberFormatException e) {
+      return false;
+    }
   }
 }
