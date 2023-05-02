86173bd965b495b9bae13a83be705a32aca1e265
nanatic <uliamurick@gmail.com>
2023-05-03 01:20:39 +0300

    Add badNotFound test

--- app/src/main/java/c2/search/netlas/target/empire/Empire.java
+++ app/src/main/java/c2/search/netlas/target/empire/Empire.java
@@ -4,13 +4,10 @@ import c2.search.netlas.annotation.Detect;
 import c2.search.netlas.annotation.Test;
 import c2.search.netlas.annotation.Wire;
 import c2.search.netlas.scheme.Host;
-import c2.search.netlas.scheme.Response;
-import c2.search.netlas.scheme.Version;
 import c2.search.netlas.target.NetlasWrapper;
 import c2.search.netlas.target.bruteratel.Utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
@@ -84,5 +81,13 @@ public class Empire {
         final String sha256doc =
                 "e87aa3bc0789083c0b05e040bdc309de0e79fc2eb12b8c04e853b1e8a4eac4f4";
         return Utils.testEndpoint(host.toString() + "/openapi.json", result, sha256doc);
+    }
+
+    @Test(extern = true)
+    public boolean badNotFound() throws IOException, NoSuchAlgorithmException {
+        final int success = 200;
+        final String sha256notfound =
+                "48c3843f57eceab2613c42ed65bff6d8701b3cfe12534220702cf2a895e45a79";
+        return Utils.testEndpoint(host.toString(), success, sha256notfound);
     }
 }
