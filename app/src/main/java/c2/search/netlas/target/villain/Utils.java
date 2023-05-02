7d01731d9436613f39102358e7cefbcd6d848e14
nanatic <uliamurick@gmail.com>
2023-05-03 01:10:34 +0300

    Fix mistake 3

--- app/src/main/java/c2/search/netlas/target/villain/Utils.java
+++ app/src/main/java/c2/search/netlas/target/villain/Utils.java
@@ -1,21 +1,11 @@
 package c2.search.netlas.target.villain;
 
 import c2.search.netlas.scheme.Host;
-import c2.search.netlas.target.NetlasWrapper;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
-import java.util.Arrays;
-import java.util.List;
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
@@ -30,60 +20,6 @@ final class Utils {
 
   private Utils() {}
 
-  public static boolean verifyCertFieldsSubject(
-      final NetlasWrapper netlasWrapper, final String[] subjectFields)
-      throws JsonMappingException, JsonProcessingException {
-    final List<List<String>> subject =
-        Arrays.asList(
-            netlasWrapper.getCertSubjectCountry(),
-            netlasWrapper.getCertSubjectProvince(),
-            netlasWrapper.getCertSubjectLocality(),
-            netlasWrapper.getCertSubjectOrganization(),
-            netlasWrapper.getCertSubjectOrganizationUnit(),
-            netlasWrapper.getCertSubjectCommonName());
-    return allFieldsEqual(subject, subjectFields);
-  }
-
-  public static boolean verifyCertFieldsIssuer(
-      final NetlasWrapper netlasWrapper, final String[] issuerFields)
-      throws JsonMappingException, JsonProcessingException {
-    final List<List<String>> issuer =
-        Arrays.asList(
-            netlasWrapper.getCertIssuerCountry(),
-            netlasWrapper.getCertIssuerProvince(),
-            netlasWrapper.getCertIssuerLocality(),
-            netlasWrapper.getCertIssuerOrganization(),
-            netlasWrapper.getCertIssuerOrganizationUnit(),
-            netlasWrapper.getCertIssuerCommonName());
-    return allFieldsEqual(issuer, issuerFields);
-  }
-
-  private static boolean allFieldsEqual(final List<List<String>> fields, final String[] values) {
-    for (int i = 0; i < values.length; i++) {
-      final String value = values[i];
-      if (!value.isEmpty() && !fields.get(i).contains(value)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  public static String getSocketResponse(final Host host) throws IOException {
-    try (Socket socket = new Socket(host.getTarget(), host.getPort())) {
-      final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      final StringBuilder response = new StringBuilder();
-      String line;
-      while (true) {
-        line = in.readLine();
-        if (line == null) {
-          break;
-        }
-        response.append(line);
-      }
-      return response.toString();
-    }
-  }
-
   public static String sendPostRequest(final Host host, final String path)
       throws IOException, NoSuchAlgorithmException, KeyManagementException {
     String url = "https://" + host.getTarget() + ":" + host.getPort() + path;
@@ -93,13 +29,13 @@ final class Utils {
       final Request request = new Request.Builder().url(url).build();
       final Response response = client.newCall(request).execute();
       responseString = response.body().string();
-    } catch (final IOException e) {
+    } catch (IOException e) {
       url = "http://" + host.getTarget() + ":" + host.getPort() + path;
       try {
         final Request request = new Request.Builder().url(url).build();
         final Response response = client.newCall(request).execute();
         responseString = response.body().string();
-      } catch (final IOException ex) {
+      } catch (IOException ex) {
         if (LOGGER.isWarnEnabled()) {
           LOGGER.warn("Failed to connect to " + url, ex);
         }
@@ -111,40 +47,30 @@ final class Utils {
   private static OkHttpClient getUnsafeOkHttpClient() {
     OkHttpClient okHttpClient = null;
     try {
-      final TrustManager[] trustAllCerts =
-          new TrustManager[] {
-            new X509TrustManager() {
-              @Override
-              public void checkClientTrusted(
-                  final java.security.cert.X509Certificate[] chain, final String authType)
-                  throws CertificateException {}
+      final TrustManager[] trustAllCerts = {
+        new X509TrustManager() {
+          @Override
+          public void checkClientTrusted(
+              final java.security.cert.X509Certificate[] chain, final String authType)
+              throws CertificateException {}
 
-              @Override
-              public void checkServerTrusted(
-                  final java.security.cert.X509Certificate[] chain, final String authType)
-                  throws CertificateException {}
+          @Override
+          public void checkServerTrusted(
+              final java.security.cert.X509Certificate[] chain, final String authType)
+              throws CertificateException {}
 
-              @Override
-              public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return new java.security.cert.X509Certificate[] {};
-              }
-            }
-          };
-
+          @Override
+          public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+            return new java.security.cert.X509Certificate[] {};
+          }
+        },
+      };
       final SSLContext sslContext = SSLContext.getInstance("SSL");
       sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
       final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
       final OkHttpClient.Builder builder = new OkHttpClient.Builder();
       builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
-      builder.hostnameVerifier(
-          new HostnameVerifier() {
-            @Override
-            public boolean verify(final String hostname, final SSLSession session) {
-              return true;
-            }
-          });
-
+      builder.hostnameVerifier((hostname, session) -> true);
       okHttpClient = builder.build();
     } catch (NoSuchAlgorithmException | KeyManagementException e) {
       if (LOGGER.isWarnEnabled()) {
