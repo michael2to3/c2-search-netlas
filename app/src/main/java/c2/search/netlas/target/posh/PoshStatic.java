e887b364b8131306b7e8dc97f17e914c09f3e3fb
Michael <michael2ga3@gmail.com>
2023-05-09 22:27:11 +0300

    feat(posh): add static data
    This commit adds static data to the Posh target. It includes a new class, `PoshStatic`, which implements the `StaticData` interface, as well as changes to the `Posh` class to remove unused imports and test methods. The `PoshStatic` class provides lists of JARM fingerprints, certificates, server ports, and HTTP headers, and SHA-256 hashes of response bodies.

---
+++ app/src/main/java/c2/search/netlas/target/posh/PoshStatic.java
@@ -0,0 +1,66 @@
+package c2.search.netlas.target.posh;
+
+import c2.search.netlas.analyze.StaticData;
+import c2.search.netlas.annotation.Static;
+import java.util.List;
+import netlas.java.scheme.Certificate;
+import netlas.java.scheme.Headers;
+import netlas.java.scheme.Issuer;
+import netlas.java.scheme.Subject;
+
+@Static(name = "Posh")
+public class PoshStatic implements StaticData {
+  @Override
+  public List<String> getJarm() {
+    final String jarm = "2ad2ad0002ad2ad22c42d42d000000faabb8fd156aa8b4d8a37853e1063261";
+    return List.of(jarm);
+  }
+
+  @Override
+  public List<Certificate> getCertificate() {
+    final var country = List.of("US");
+    final var province = List.of("Minnesota");
+    final var locality = List.of("Minnetonka");
+    final var organization = List.of("Pajfds");
+    final var organizationUnit = List.of("Jethpro");
+    final var commonName = List.of("P18055077");
+    Subject subject = new Subject();
+    subject.setCountry(country);
+    subject.setProvince(province);
+    subject.setLocality(locality);
+    subject.setOrganization(organization);
+    subject.setOrganizationalUnit(organizationUnit);
+    subject.setCommonName(commonName);
+    Issuer issue = new Issuer();
+    issue.setCountry(country);
+    issue.setProvince(province);
+    issue.setLocality(locality);
+    issue.setOrganization(organization);
+    issue.setOrganizationalUnit(organizationUnit);
+    issue.setCommonName(commonName);
+
+    Certificate cert = new Certificate();
+    cert.setSubject(subject);
+    cert.setIssuer(issue);
+
+    return List.of(cert);
+  }
+
+  @Override
+  public List<Integer> getPort() {
+    return null;
+  }
+
+  @Override
+  public List<Headers> getHeader() {
+    Headers header = new Headers();
+    header.setHeader("Content-Type", List.of("text/html"));
+    header.setHeader("Server", List.of("Apache"));
+    return List.of(header);
+  }
+
+  @Override
+  public List<String> getBodyAsSha256() {
+    return List.of("f8bf41177a5f5e808a7ccb648b51080b031f15ca8018d91a576263d6cc626eb6");
+  }
+}
